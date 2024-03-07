30dadf75945ad1822b064257b94cca2a62a2dabb
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2024-03-07 02:26:40 -0300

    Iniciei a construção das sections do site institucional

---
+++ Sprint2Semestre/site/public/js/index.js
@@ -0,0 +1,16 @@
+function changeNav() {
+
+    const navbar = document.querySelector('nav');
+
+    addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = '#001736';
+            navbar.style.fontSize = '1em';
+        } else {
+            navbar.style.backgroundColor = 'transparent';
+            navbar.style.fontSize = '1.2em';
+
+        }
+        console.log(window.scrollY);
+    });
+}
