a41d9d2c9c27a97065a0cb3414b0d39f3101339b
leonardoodsilva <leonardo.osilva@sptech.school>
2024-05-26 13:42:38 -0300

    Alterei informações da tela projetos e testei inserção de gráfico

---
+++ Re-Data/site/public/js/projetos.js
@@ -0,0 +1,40 @@
+// const ctx = document.getElementById('myChart');
+// console.log(ctx);
+// new Chart(ctx, {
+//     type: 'doughnut',
+//     data: {
+//         labels: ['Total Máquinas', 'Atenção', 'Perigo'],
+//         datasets: [
+//         {
+//             label: 'Máquinas',
+//             data: [20, 0, 2],
+//             backgroundColor: '#FDAD00',
+//             borderColor: 'white',
+//             borderWidth: 2,
+//             fill: true
+//         }]
+//     },
+//     options: {
+//         maintainAspectRatio: false,
+//         scales: {
+//             y: {
+//                 beginAtZero: true,
+//             },
+//         },
+//         plugins: {
+//             title: {
+//                 display: true,
+//                 text: "Quantidade de máquinas alocadas",
+//                 color: 'white',
+//                 font: {
+//                     size: 10,
+//                 }
+//             }
+//         },
+//         elements: {
+//             line: {
+//                 cubicInterpolationMode: 'monotone'
+//             }
+//         }
+//     }
+// });
