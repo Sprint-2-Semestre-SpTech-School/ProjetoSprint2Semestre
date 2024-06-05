3648e05ee95e72ea53c63e1af224e304d0485f9f
Sabrina <sabrina.tulio@sptech.school>
2024-06-05 12:27:44 -0300

    adicionando modal para cadastro de usb

--- Re-Data/site/public/js/dispositivoUsb.js
+++ Re-Data/site/public/js/dispositivoUsb.js
@@ -15,3 +15,28 @@ function sairConta() {
         window.location = "login.html";
     }, "1000");
 }
+
+// Formulario
+document.addEventListener('DOMContentLoaded', function () {
+    // Esconde o formulário quando a página carregar
+    document.getElementById('formularioAdicionar').classList.add('hidden');
+
+    // Exibe o formulário quando o botão "Novo" for clicado
+    document.querySelector('.buttonTools').addEventListener('click', function () {
+        document.getElementById('formularioAdicionar').classList.remove('hidden');
+    });
+
+    // Esconde o formulário quando o botão "Salvar" for clicado
+    document.getElementById('saveButton').addEventListener('click', function (event) {
+        event.preventDefault(); // Impede o envio do formulário para demonstração
+        document.getElementById('formularioAdicionar').classList.add('hidden');
+    });
+
+    // Exibe o formulário quando o botão de editar for clicado
+    document.querySelectorAll('.edit').forEach(function (button) {
+        button.addEventListener('click', function () {
+            document.getElementById('formularioAdicionar').classList.remove('hidden');
+            // Aqui você pode adicionar o código para preencher o formulário com os dados do usuário a ser editado
+        });
+    });
+});
