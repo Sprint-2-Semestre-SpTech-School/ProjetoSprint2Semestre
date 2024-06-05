231d718922344258af89976c485b73dda51fafb7
Sabrina <sabrina.tulio@sptech.school>
2024-06-05 01:56:51 -0300

    criando tela de dispositivos USB

---
+++ Re-Data/site/public/js/dispositivoUsb.js
@@ -0,0 +1,17 @@
+// Modal para sair
+function sair() {
+    const modalSairDentro = document.getElementById('modalSair')
+    modalSairDentro.classList.add('abrir')
+
+    modalSairDentro.addEventListener('click', (e) => {
+        if (e.target.id == 'fecharModal' || e.target.id == 'modalSair') {
+            modalSairDentro.classList.remove('abrir')
+        }
+    })
+}
+
+function sairConta() {
+    setTimeout(() => {
+        window.location = "login.html";
+    }, "1000");
+}
